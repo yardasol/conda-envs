b001e5e41eee616186c9b42cfd17e26738685e71
yardasol <yardasol@grinnell.edu>
2022-02-10 15:31:51 -0600

    build pyan from git repo

--- viz-env.sh
+++ viz-env.sh
@@ -6,3 +6,6 @@ conda config --env --append channels conda-forge
 conda config --env --set pip_interop_enabled True
 
 mamba env update -f viz-environment.yml
+
+pip install git+https://github.com/Technologicat/pyan.git
+
